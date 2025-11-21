bc7f89e92194a970900d7b697a4c2e5a0b9bca7f
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 06:50:30 +0700

    Update task.rb

--- task_manager/task.rb
+++ task_manager/task.rb
@@ -1,1 +1,20 @@
+class Task
+  attr_accessor :id, :title, :description, :completed
+
+  def initialize(id, title, description, completed = false)
+    @id = id
+    @title = title
+    @description = description
+    @completed = completed
+  end
+
+  def to_h
+    {
+      id: @id,
+      title: @title,
+      description: @description,
+      completed: @completed
+    }
+  end
+end
 
