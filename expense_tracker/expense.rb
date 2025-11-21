842ff805d874fba0b026730e57f85a79dcf5abd9
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 06:57:24 +0700

    Update expense.rb

--- expense_tracker/expense.rb
+++ expense_tracker/expense.rb
@@ -1,1 +1,15 @@
+class Expense
+  attr_accessor :id, :amount, :category, :note
+
+  def initialize(id, amount, category, note)
+    @id = id
+    @amount = amount.to_f
+    @category = category
+    @note = note
+  end
+
+  def to_row
+    [@id, @amount, @category, @note]
+  end
+end
 
