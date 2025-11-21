c3ca12e1a86b0d6ed8a8ba47f23e1e7d97dc8c65
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 06:57:49 +0700

    Update analytics.rb

--- expense_tracker/analytics.rb
+++ expense_tracker/analytics.rb
@@ -1,1 +1,16 @@
+class Analytics
+  def self.by_category(expenses)
+    totals = Hash.new(0)
+    expenses.each { |e| totals[e.category] += e.amount }
+    totals
+  end
+
+  def self.chart(hash)
+    puts "\n=== ASCII EXPENSE CHART ==="
+    hash.each do |cat, val|
+      bar = "#" * (val / 5).to_i
+      puts "#{cat.ljust(10)} | #{bar} #{val.round(2)}"
+    end
+  end
+end
 
