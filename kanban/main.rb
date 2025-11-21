e6c930d48479e666b3141b7e672aa91604934c82
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 06:59:54 +0700

    Update main.rb

--- kanban/main.rb
+++ kanban/main.rb
@@ -1,1 +1,36 @@
+require_relative 'storage'
+require_relative 'board'
+
+data = Storage.load
+board = Board.new(data)
+
+loop do
+  puts "\n=== KANBAN BOARD ==="
+  board.columns.each do |k, v|
+    puts "#{k.upcase}: #{v.join(', ')}"
+  end
+
+  puts "\n1. Add Task"
+  puts "2. Move Task"
+  puts "3. Exit"
+  print "> "
+  c = gets.chomp
+
+  case c
+  when "1"
+    print "Title: "; t = gets.chomp
+    board.add_task(t)
+    Storage.save(board.columns)
+
+  when "2"
+    print "Task: "; t = gets.chomp
+    print "From(todo/doing/done): "; f = gets.chomp
+    print "To(todo/doing/done): "; to = gets.chomp
+    board.move(t, f, to)
+    Storage.save(board.columns)
+
+  when "3"
+    exit
+  end
+end
 
