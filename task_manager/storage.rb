a23c27e6af4e47b84d070e7ed98903d7f47914e5
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 06:50:43 +0700

    Update storage.rb

--- task_manager/storage.rb
+++ task_manager/storage.rb
@@ -1,1 +1,18 @@
+require 'json'
+require_relative 'task'
+
+class Storage
+  FILE_PATH = "tasks.json"
+
+  def self.load
+    return [] unless File.exist?(FILE_PATH)
+    data = JSON.parse(File.read(FILE_PATH))
+    data.map { |t| Task.new(t["id"], t["title"], t["description"], t["completed"]) }
+  end
+
+  def self.save(tasks)
+    arr = tasks.map(&:to_h)
+    File.write(FILE_PATH, JSON.pretty_generate(arr))
+  end
+end
 
