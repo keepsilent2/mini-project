a83df0d34f1acfc1b113ef36aed3c00438e43128
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 06:59:26 +0700

    Update storage.rb

--- kanban/storage.rb
+++ kanban/storage.rb
@@ -1,1 +1,17 @@
+require 'yaml'
+
+class Storage
+  FILE = "board.yml"
+
+  def self.load
+    unless File.exist?(FILE)
+      return {"todo" => [], "doing" => [], "done" => []}
+    end
+    YAML.load_file(FILE)
+  end
+
+  def self.save(data)
+    File.write(FILE, data.to_yaml)
+  end
+end
 
