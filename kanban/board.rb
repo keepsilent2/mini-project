4323d3405f0685d219bfe6e8cb1d794d519090f0
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 06:59:09 +0700

    Update board.rb

--- kanban/board.rb
+++ kanban/board.rb
@@ -1,1 +1,18 @@
+class Board
+  attr_accessor :columns
+
+  def initialize(data)
+    @columns = data
+  end
+
+  def add_task(title)
+    @columns["todo"] << title
+  end
+
+  def move(task, from, to)
+    return unless @columns[from].include?(task)
+    @columns[from].delete(task)
+    @columns[to] << task
+  end
+end
 
