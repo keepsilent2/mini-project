3442013860e3b1759e9a5ceaf99a7d3b7ca5f710
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 07:08:23 +0700

    Update parser.rb

--- log_analyzer/parser.rb
+++ log_analyzer/parser.rb
@@ -1,1 +1,16 @@
+class LogParser
+  def initialize(file)
+    @file = file
+  end
+
+  def parse
+    logs = []
+    File.readlines(@file).each do |line|
+      if line =~ /\[(INFO|WARN|ERROR)\] (.+)/
+        logs << { level: $1, message: $2 }
+      end
+    end
+    logs
+  end
+end
 
