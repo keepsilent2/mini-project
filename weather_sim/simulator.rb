52c9e921cdd62d7a2c480dfec3a27d772453d3da
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 07:04:14 +0700

    Update simulator.rb

--- weather_sim/simulator.rb
+++ weather_sim/simulator.rb
@@ -1,1 +1,14 @@
+class WeatherSimulator
+  def initialize(days)
+    @days = days
+  end
+
+  def generate
+    (1..@days).map do |d|
+      temp = rand(20..35)
+      humidity = rand(40..90)
+      { day: d, temp: temp, humidity: humidity }
+    end
+  end
+end
 
