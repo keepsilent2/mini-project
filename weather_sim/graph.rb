043b4daee68c6ed5f4f52a4feb0c947ca5ac21d2
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 07:04:25 +0700

    Update graph.rb

--- weather_sim/graph.rb
+++ weather_sim/graph.rb
@@ -1,1 +1,10 @@
+class Graph
+  def self.line(data, key)
+    max = data.map { |d| d[key] }.max
+    data.each do |d|
+      bar = "*" * (d[key] * 50 / max)
+      puts "Day #{d[:day]} | #{bar} (#{d[key]})"
+    end
+  end
+end
 
