c50e5b3238913df045c8965898fe738668d161ba
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 07:01:22 +0700

    Update generator.rb

--- weather_sim/generator.rb
+++ weather_sim/generator.rb
@@ -1,1 +1,16 @@
+class WeatherGenerator
+  CONDITIONS = ["Sunny", "Rain", "Cloudy", "Storm", "Windy"]
+
+  def self.random_day
+    {
+      temp: rand(18..34),
+      humidity: rand(30..90),
+      condition: CONDITIONS.sample
+    }
+  end
+
+  def self.week
+    (1..7).map { random_day }
+  end
+end
 
