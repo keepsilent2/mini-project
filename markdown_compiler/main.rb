df538637a80ca3751550b155eee8f5a7c804c222
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 07:03:10 +0700

    Update main.rb

--- markdown_compiler/main.rb
+++ markdown_compiler/main.rb
@@ -1,6 +1,19 @@
-require 'fileutils'
-require_relative 'compiler'
+require_relative "compiler"
 
-compiler = Compiler.new
-compiler.compile_all
+template = File.read("template.html")
+compiler = MarkdownCompiler.new(template)
 
+Dir.mkdir("output") unless Dir.exist?("output")
+
+Dir.glob("notes/*.md").each do |file|
+  md = File.read(file)
+  compiled = compiler.compile(md)
+
+  title = md.lines.first.gsub("#", "").strip
+  html = compiler.build_page(title, compiled)
+
+  out_name = file.split("/").last.gsub(".md", ".html")
+  File.write("output/#{out_name}", html)
+end
+
+puts "Compiled successfully!"
