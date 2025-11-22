a9ff434d9f3c115e42aa4df7b9e1e6882fb4e688
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 07:08:36 +0700

    Update report.rb

--- log_analyzer/report.rb
+++ log_analyzer/report.rb
@@ -1,1 +1,14 @@
+class Report
+  def self.summary(logs)
+    count = Hash.new(0)
+    logs.each { |l| count[l[:level]] += 1 }
+    count
+  end
+
+  def self.display(count)
+    count.each do |k, v|
+      puts "#{k}: #{v}"
+    end
+  end
+end
 
