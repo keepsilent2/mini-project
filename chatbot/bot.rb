24dd0c09f755dc1203818435b3c375d787043138
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 07:09:58 +0700

    Update bot.rb

--- chatbot/bot.rb
+++ chatbot/bot.rb
@@ -1,1 +1,20 @@
+require "yaml"
+
+class Bot
+  def initialize
+    @intents = YAML.load_file("intents.yml")
+  end
+
+  def respond(text)
+    text = text.downcase
+
+    @intents.each do |key, data|
+      data["patterns"].each do |p|
+        return data["responses"].sample if text.include?(p)
+      end
+    end
+
+    "I don't understand."
+  end
+end
 
