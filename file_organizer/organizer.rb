8af90efc826971b3367e36812ad546889e551d69
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 07:11:28 +0700

    Update organizer.rb

--- file_organizer/organizer.rb
+++ file_organizer/organizer.rb
@@ -1,1 +1,25 @@
+require "json"
+require "fileutils"
+
+class Organizer
+  def initialize(folder)
+    @folder = folder
+    @rules = JSON.parse(File.read("rules.json"))
+  end
+
+  def organize
+    Dir.glob("#{@folder}/*").each do |file|
+      next if File.directory?(file)
+
+      ext = file.split(".").last.downcase
+
+      @rules.each do |folder, exts|
+        if exts.include?(ext)
+          FileUtils.mkdir_p("#{@folder}/#{folder}")
+          FileUtils.mv(file, "#{@folder}/#{folder}/")
+        end
+      end
+    end
+  end
+end
 
