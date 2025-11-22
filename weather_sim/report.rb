6b3200db9b4362531bcbdacd4286a35d1615c68f
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 07:01:32 +0700

    Update report.rb

--- weather_sim/report.rb
+++ weather_sim/report.rb
@@ -1,1 +1,9 @@
+class Report
+  def self.print(week)
+    puts "\n=== WEEKLY WEATHER REPORT ==="
+    week.each_with_index do |d, i|
+      puts "Day #{i+1}: #{d[:temp]}°C | #{d[:humidity]}% | #{d[:condition]}"
+    end
+  end
+end
 
