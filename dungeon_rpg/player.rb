43bc53b18eddcd2d0e7247f868bfc889ecde94db
Keepsilent <dontspeak14e@gmail.com>
2025-11-22 07:05:36 +0700

    Update player.rb

--- dungeon_rpg/player.rb
+++ dungeon_rpg/player.rb
@@ -1,1 +1,14 @@
+class Player
+  attr_accessor :hp, :inventory, :name
+
+  def initialize(name)
+    @name = name
+    @hp = 100
+    @inventory = []
+  end
+
+  def attack
+    rand(10..20)
+  end
+end
 
